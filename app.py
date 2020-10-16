680f6b23f385f0bf1a1e1fe424dcf38ee413d30a
Hidir Bolaji <hidirbolaji93@gmail.com>
2020-10-16 07:57:23 +0100

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,18 @@ user_input2 = 2
 
 user_input3 = 3
 
+
+
+
 if __name__ == '__main__':
+  
+  
+    for player in players:
+      if player['experience'] == 'YES':
+        player['experience'] = True
+
+    else:
+        player['experience'] = False
 
     for char in y:
         char['height'] = char['height'].replace(' inches', '')
@@ -25,16 +36,7 @@ if __name__ == '__main__':
         char['guardians'] = char['guardians'].replace(" and ", ', ')
         char['guardians'] = ''.join(char['guardians'])
     
-    
-    
-    
-    for player in players:
-        if player['experience'] == 'YES':
-            player['experience'] = True
-    
-        else:
-            player['experience'] = False
-            
+      
     
     def start():
     
@@ -62,114 +64,115 @@ if __name__ == '__main__':
                 option2 = int(input('\nChoose a team > '))     
                 total_players = int(len(players) / len(teams))
                 
-                
                 pn = 0
-                player_names = []
                 
                 hl = 0
-                heights_list = []
                 
                 pe=0
-                players_experience = []
                 
                 gl=0
+                
+                
+                player_names = []
+
+                heights_list = []
+                
+                players_experience = []
+                
                 guardians_list = []
                 
+                
+                def info():
+                  print('\nTeam: {} Stats:'.format(teams[0]))
+                  print('heights_list: {}'.format(heights_list))
+                  print('\nTotal Players: {}'.format(total_players))
+                  print('\nTotal experienced: {}'.format(len(players_experience) -players_experience.count(False)))
+                  print('\nTotal inexperienced: {}'.format(len(players_experience) - players_experience.count(True)))
+                  print('\nAverage height: {}'.format(sum(heights_list)/len(heights_list)))
+                  print('\nPlayers on team: ',', '.join(player_names))
+                  print('\nGuardians: ',', '.join(guardians_list))
+                  
+                  
                 while option2 == user_input1:
-                    
-                    player_names.append(players[pn]['name'])
+                    player_names.append(y[pn]['name'])
                     pn += 1
                 
-                    heights_list.append(players[hl]['height'])
+                    heights_list.append(y[hl]['height'])
                     hl +=1
                 
-                    players_experience.append(players[pe]['experience'])
+                    players_experience.append(y[pe]['experience'])
                     pe += 1
                 
-                    guardians_list.append(players[gl]['guardians'])
+                    guardians_list.append(y[gl]['guardians'])
                     gl += 1
+
                     if gl==6:
                         break  
                 
                 if option2 == user_input1:  
-                    print('\nTeam: {} Stats:'.format(teams[0]))
-                    print('heights_list: {}'.format(heights_list))
-                    print('\nTotal Players: {}'.format(total_players))
-                    print('\nTotal experienced: {}'.format(len(players_experience) - players_experience.count(False)))
-                    print('\nTotal inexperienced: {}'.format(len(players_experience) - players_experience.count(True)))
-                    print('\nAverage height: {}'.format(sum(heights_list)/len(heights_list)))
-                    print('\nPlayers on team:\n ',', '.join(player_names))
-                    print('\nGuardians: ',', '.join(guardians_list))
-                
+                  info()
+                  
                 
                 pn = 6
                 hl = 6
                 pe = 6
                 gl = 6
+                
+                
                 while option2 == user_input2:
-                    player_names.append(players[pn]['name'])
+                    player_names.append(y[pn]['name'])
                     pn += 1
                     
-                    heights_list.append(players[hl]['height'])
+                    heights_list.append(y[hl]['height'])
                     hl +=1
                 
-                    players_experience.append(players[pe]['experience'])
+                    players_experience.append(y[pe]['experience'])
                     pe += 1
                 
-                    guardians_list.append(players[gl]['guardians'])
+                    guardians_list.append(y[gl]['guardians'])
                     gl += 1
                     if gl==12:
                         break   
                     
                 
                 if option2 == user_input2:
-                    print('\nTeam: {} Stats:'.format(teams[1]))
-                    print('heights_list: {}'.format(heights_list))
-                    print('\nTotal Players: {}'.format(total_players))
-                    print('\nTotal experienced: {}'.format(len(players_experience) - players_experience.count(False)))
-                    print('\nTotal inexperienced: {}'.format(len(players_experience) - players_experience.count(True)))
-                    print('\nAverage height: {}'.format(sum(heights_list)/len(heights_list)))
-                    print('\nPlayers on team:\n ',', '.join(player_names))
-                    print('\nGuardians: ',', '.join(guardians_list))
-                
-                
+                  info()
                 
                 pn = 12
                 hl = 12
                 pe = 12
                 gl = 12
+                
+                
                 while option2 == user_input3:
-                    player_names.append(players[pn]['name'])
+                    player_names.append(y[pn]['name'])
                     pn += 1
                 
-                    heights_list.append(players[hl]['height'])
-                    hl +=1
+                    heights_list.append(y[hl]['height'])
+                    hl += 1
                 
-                    players_experience.append(players[pe]['experience'])
+                    players_experience.append(y[pe]['experience'])
                     pe += 1
                 
-                    guardians_list.append(players[gl]['guardians'])
+                    guardians_list.append(y[gl]['guardians'])
                     gl += 1
                     if gl==18:
                         break  
                 
                 if option2 == user_input3:
-                    print('\nTeam: {} Stats:'.format(teams[2]))
-                    print('heights_list: {}'.format(heights_list))
-                    print('\nTotal Players: {}'.format(total_players))
-                    print('\nTotal experienced: {}'.format(len(players_experience) - players_experience.count(False)))
-                    print('\nTotal inexperienced: {}'.format(len(players_experience) - players_experience.count(True)))
-                    print('\nAverage height: {}'.format(sum(heights_list)/len(heights_list)))
-                    print('\nPlayers on team:\n ',', '.join(player_names))
-                    print('\nGuardians: ',', '.join(guardians_list))
-                    
+                  info()
+
+                
+                
                 elif option1 > user_input3:
                     print('\nPlease enter a number between 1 & 3 ')
                     continue
                     
                 elif option1 < user_input1:
                     print('\nPlease enter a number between 1 & 3 ')
-                    continue    
+                    continue
+                
+            
                     
             except ValueError:
                 print ('\nError!!!. Please enter a number withing provided range')
